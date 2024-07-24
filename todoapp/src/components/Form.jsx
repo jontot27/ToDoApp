d57d955c164119e43610b2e50f4db24c5575e8f5
jontot27 <j.a.ballesterosjr@gmail.com>
2024-07-24 19:01:37 +0800

    Styling The Form

--- todoapp/src/components/Form.jsx
+++ todoapp/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import style from "./form.module.css";
 export default function Form({ toDos, setTodos }) {
   const [toDo, setTodo] = useState("");
 
@@ -8,13 +9,19 @@ export default function Form({ toDos, setTodos }) {
     setTodo("");
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        onChange={(e) => setTodo(e.target.value)}
-        value={toDo}
-        type="text"
-      ></input>
-      <button type="submit">Add Task</button>
+    <form className={style.todoForm} onSubmit={handleSubmit}>
+      <div className={style.inputContainer}>
+        <input
+          className={style.modernInput}
+          onChange={(e) => setTodo(e.target.value)}
+          value={toDo}
+          type="text"
+          placeholder="Type task here..."
+        ></input>
+        <button className={style.modernButton} type="submit">
+          Add Task
+        </button>
+      </div>
     </form>
   );
 }
