18429a8d30a9a92f9ce41567ded933e53a693165
jontot27 <j.a.ballesterosjr@gmail.com>
2024-07-31 19:59:45 +0800

    Sorting Items In A Todo List

--- todoapp/src/components/Footer.jsx
+++ todoapp/src/components/Footer.jsx
@@ -1,18 +1,14 @@
 import style from "./footer.module.css";
 export default function Footer({ completedTodos, totalTodos }) {
   return (
-    <div>
-      <div className={style.footerCompletedTask}>
-        <span>
-          <h2 className={style.completedTaskText}>Completed Task:</h2>
-          <h1 className={style.completedTaskNumber}>{completedTodos}</h1>
-        </span>
+    <div className={style.footerCompletedTask}>
+      <div>
+        <h2 className={style.completedTaskText}>Completed Task:</h2>
+        <h1 className={style.completedTaskNumber}>{completedTodos}</h1>
       </div>
-      <div className={style.footerTasksNumber}>
-        <span>
-          <h2 className={style.completedTaskText}>Number of Tasks:</h2>
-          <h1 className={style.completedTaskNumber}>{totalTodos}</h1>
-        </span>
+      <div>
+        <h2 className={style.completedTaskText}>Number of Tasks:</h2>
+        <h1 className={style.completedTaskNumber}>{totalTodos}</h1>
       </div>
     </div>
   );
--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -2,9 +2,12 @@ import TodoItems from "./TodoItems";
 import style from "./todolist.module.css";
 
 export default function TodoList({ toDos, setTodos }) {
+  const sortedTodos = toDos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={style.list}>
-      {toDos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItems
           key={item.name}
           item={item}
